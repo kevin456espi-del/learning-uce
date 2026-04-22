abd07ba560063348d0b3f9b16515d112b0cf7af9
kevin456espi-del <kevin456espi@gmail.com>
2026-04-21 21:09:14 -0500

    Update pcei2a-actividad3.py

--- APE-PAE/pcei2a-actividad3.py
+++ APE-PAE/pcei2a-actividad3.py
@@ -1,2 +1,24 @@
-# Taller grupal para la resolución de problemas cotidianos
-# mediante el uso de listas y sus métodos en Phyton.
+lista=[]
+filas=int(input("ingrese numero de filas: "))
+columnas=int(input("ingrese numero de columnas:" ))
+
+for i in range(filas):
+    fila=[]
+    for j in range (columnas):
+        while True:
+            fi=int(input(f"ingrese un valor {i}{j}: "))
+            if fi % 2 == 0:
+                fila.append(fi)
+                break
+            else:
+                print("ingrese un numero par")
+    lista.append(fila)
+
+for i in lista:
+    lista.sort()
+    for j in i:
+        i.sort()
+        
+for i in lista:
+    print(i)
+  si verdad?
